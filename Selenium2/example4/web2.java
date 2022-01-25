afe8a6530ede0a6dbc4672d484cbaeaf36330964
supriya-mallick <supriyamallick132@gmail.com>
2022-01-25 04:40:40 -0800

    First Commit

---
+++ Selenium2/example4/web2.java
@@ -0,0 +1,12 @@
+package example4;
+
+import org.testng.annotations.Test;
+
+public class web2 {
+  @Test
+  public void f() {
+	  System.setProperty("webdriver.chrome.driver","C:\\Users\\supriya\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
+	  webDriver d1;
+	  
+  }
+}
